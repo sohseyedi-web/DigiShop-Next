31fe2c8834e97a26a8b789b199c7c034bc0d08f5
sohseyedi <sohseyedi@gmail.com>
2024-01-04 17:15:37 +0330

    user content

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,10 +1,20 @@
-
+import Link from "next/link";
 
 const Header = () => {
-
-
   return (
-    <header>soheil</header>
+    <header className="w-full py-4 border-b border-gray-400 shadow">
+      <nav className="flex items-center justify-between max-w-7xl mx-auto container lg:px-0 px-3">
+        <div className="flex items-center gap-x-3">
+          <h3 className="text-2xl font-semibold text-gray-800">دیجی شاپ</h3>
+          <Link href={"/"}>خانه</Link>
+          <Link href={"/products"}>فروشگاه</Link>
+          <Link href={"/"}>ارتباط با ما</Link>
+        </div>
+        <Link href={"/auth"} className="btn btn--primary">
+          ورود / ثبت نام
+        </Link>
+      </nav>
+    </header>
   );
 };
 
