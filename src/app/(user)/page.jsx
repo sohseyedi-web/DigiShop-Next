136c528b7b9bef434cda866aa3d5de974c460d8f
sohseyedi <sohseyedi@gmail.com>
2024-01-07 11:32:43 +0330

    change project layout

--- src/app/(user)/page.jsx
+++ src/app/(user)/page.jsx
@@ -1,9 +1,6 @@
-import HeaderLayout from "@/components/Header";
-
 export default function Home() {
   return (
     <main className="text-3xl">
-      <HeaderLayout />
       <div>صفحه خانه</div>
     </main>
   );
