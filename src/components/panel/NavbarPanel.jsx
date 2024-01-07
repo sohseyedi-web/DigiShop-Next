3caf882483c5a7a588a8cd17d77c1c0d2270c462
sohseyedi <sohseyedi@gmail.com>
2024-01-07 18:25:15 +0330

    admin data

--- src/components/panel/NavbarPanel.jsx
+++ src/components/panel/NavbarPanel.jsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 import * as RiIcon from "react-icons/ri";
 import { useActiveSidebar } from "@/hooks/useActiveSidebar";
 import { useAuth } from "@/hooks/useAuth";
 
 const NavbarPanel = () => {
-  const { setActive,active } = useActiveSidebar();
+  const { setActive, active } = useActiveSidebar();
   const { user } = useAuth();
 
   console.log(active);
 
   return (
-    <nav className="w-full flex shadow py-1 items-center justify-between text-gray-800">
+    <nav className="w-full flex border-b py-1 items-center justify-between text-gray-800">
       <h5 className="text-xl font-bold">{user?.name} , خوش اومدی</h5>
       <div className="flex items-center gap-x-4">
         <RiIcon.RiSunLine size={26} className="cursor-pointer" />
